5c58ff4a041a6667153a62d1fbc5b3de419b3835
Grzegorz Deredas <g.deredas@gmail.com>
2022-09-26 22:29:23 +0200

    some code improvements with comments

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-docReady(function () {
-  // here we are creating eyes
-
+documentReady(function () {
   // first we get how many space we have
-  let main = document.getElementById("main").getBoundingClientRect();
-  let width = main.width;
-  let howMany = width / 50; // 50 is our eye width
+  const main = document.getElementById("main").getBoundingClientRect();
+  const howMany = main.width / 50; // 50 is our eye width
 
+  // now we are adding eyes images
   var src = document.getElementById("eyes");
   for (let i = 0; i < howMany; i++) {
     const img = document.createElement("img");
-    const left = Math.random() * width;
+    const left = Math.random() * main.width;
     img.src = "eye.png";
     img.className = "eye";
     img.style.top = `${50*i}px`;
@@ -17,42 +15,47 @@ docReady(function () {
     src.appendChild(img);
   }
 
+  // now we are adding some precalulated data to each img
   const eyes = document.querySelectorAll(".eye");
   eyes.forEach((eye) => {
     const rekt = eye.getBoundingClientRect();
     eye.dataset.anchorX = rekt.left + rekt.width / 2;
     eye.dataset.anchorY = rekt.top + rekt.height / 2;
   });
+
+  // we are using throttle to reduce number of mousemove events
+  document.addEventListener("mousemove", throttle(20, (e) => {
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+
+    // for each eye we calculate angle between eye and cursor
+    // and then we rotate eye image
+    const eyes = document.querySelectorAll(".eye");
+    eyes.forEach((eye) => {
+      const angleDeg = angle(
+        mouseX,
+        mouseY,
+        eye.dataset.anchorX,
+        eye.dataset.anchorY
+      );
+
+      // 135 because out image is rotated initially
+      eye.style.transform = `rotate(${angleDeg + 135}deg)`;
+    });
+  }));
 });
 
-document.addEventListener("mousemove", (e) => {
-  const mouseX = e.clientX;
-  const mouseY = e.clientY;
-
-  const eyes = document.querySelectorAll(".eye");
-  eyes.forEach((eye) => {
-    console.log(eye.dataset.anchorX);
-    const angleDeg = angle(
-      mouseX,
-      mouseY,
-      eye.dataset.anchorX,
-      eye.dataset.anchorY
-    );
-
-    eye.style.transform = `rotate(${90 + angleDeg}deg)`;
-  });
-});
-
+// here we are calculating angle between mouse and eye
 function angle(cx, cy, ex, ey) {
   const dx = ex - cx;
   const dy = ey - cy;
   const rad = Math.atan2(dy, dx);
   const deg = (rad * 180) / Math.PI + 75;
-
   return deg;
 }
 
-function docReady(fn) {
+// we want some code to be executed after document is ready
+function documentReady(fn) {
   // see if DOM is already available
   if (
     document.readyState === "complete" ||
@@ -62,5 +65,13 @@ function docReady(fn) {
     setTimeout(fn, 1);
   } else {
     document.addEventListener("DOMContentLoaded", fn);
+  }
+}
+
+function throttle(wait, callback) {
+  var timeout
+  return function(e) {
+    if (timeout) return;
+    timeout = setTimeout(() => (callback(e), timeout=undefined), wait)
   }
 }
